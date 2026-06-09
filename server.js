e406287bfc2141b05b8dd61474d6711c52d5fe44
Shivani Anil <shivani.ka8606@gmail.com>
2026-06-09 10:20:07 +0530

    fixed server.js syntax error

--- server.js
+++ server.js
@@ -36,4 +36,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})cd "C:\Users\HP\Downloads\mern practice\INTERNSHIP\MERN PROJECT\BACKEND"
+})
